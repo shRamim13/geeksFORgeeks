77c2dce6f23a01c9aba1950a56c2bcbdf3b8feca
shRamim13 <shramim13@gmail.com>
2025-02-01 00:21:43 +0600

    s

--- Minimum Platforms.cpp
+++ Minimum Platforms.cpp
@@ -3,10 +3,10 @@ class Solution
 public:
     int findPlatform(vector<int> &arr, vector<int> &dep)
     {
-        int stationCount = 0;
+
         sort(arr.begin(), arr.end());
         sort(dep.begin(), dep.end());
-
+        int stationCount = 0;
         int n = arr.size();
         int i = 0, j = 0;
         int maxi = 0;
