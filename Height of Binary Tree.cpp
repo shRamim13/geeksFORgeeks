443e2dc59a6569dfc778bc5631424a21c48f6957
shRamim13 <shramim13@gmail.com>
2025-03-02 00:53:06 +0600

    das

---
+++ Height of Binary Tree.cpp
@@ -0,0 +1,150 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after splitting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// } Driver Code Ends
+
+// User function template for C++
+
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution
+{
+public:
+    void solve(Node *root, int cnt, int &maxlen)
+    {
+        if (!root)
+        {
+            return;
+        }
+        if (!root->left && !root->right)
+        {
+            maxlen = max(maxlen, cnt);
+            return;
+        }
+        if (root->left)
+        {
+            solve(root->left, cnt + 1, maxlen);
+        }
+        if (root->right)
+        {
+            solve(root->right, cnt + 1, maxlen);
+        }
+    }
+    int height(Node *node)
+    {
+        int maxlen = 0;
+        int cnt = 0;
+        solve(node, cnt, maxlen);
+        return maxlen;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d ", &t);
+    while (t--)
+    {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution ob;
+        cout << ob.height(root) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
